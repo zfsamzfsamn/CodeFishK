4886b004c05f0772e2dd5296b51dda0f74a52806
zfsamzfsamn <dulanqiongs359@163.com>
2022-01-25 22:05:18 +0800

    mmc && wifi_bus code
    Signed-off-by: lzl <sucer_fater@163.com>

--- model/network/wifi/bus/hdf_sdio_intf.h
+++ model/network/wifi/bus/hdf_sdio_intf.h
@@ -1,17 +1,18 @@
 /*
- * Copyright (c) 2020-2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2021 Huawei Device Co., Ltd.
  *
  * HDF is dual licensed: you can use it either under the terms of
  * the GPL, or the BSD license, at your option.
  * See the LICENSE file in the root of this repository for complete details.
  */
 
-#ifndef HDF_WLAN_SDIO_ADAPT_H
-#define HDF_WLAN_SDIO_ADAPT_H
+#ifndef HDF_SDIO_INTF_H
+#define HDF_SDIO_INTF_H
 
 #include "hdf_base.h"
-#include "hdf_wifi_product.h"
-#include "hdf_wlan_config.h"
+#include "hdf_ibus_intf.h"
+#include "hdf_log.h"
+#include "securec.h"
 
 #ifdef __cplusplus
 #if __cplusplus
@@ -19,14 +20,13 @@ extern "C" {
 #endif
 #endif
 
-int32_t HdfWlanConfigSDIO(uint8_t busId);
+int32_t HdfSdioBusInit(struct BusDev *dev, const struct HdfConfigWlanBus *busConfig);
 
 #ifdef __cplusplus
 #if __cplusplus
 }
 #endif
 #endif
-
 
 #endif
 
