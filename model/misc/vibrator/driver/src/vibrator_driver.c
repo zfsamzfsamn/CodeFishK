f310e75f40bdc75dd44f59071194da8828bc55e6
zfsamzfsamn <dulanqiongs359@163.com>
2022-04-07 23:03:08 +0800

    modify vibrator
    Signed-off-by: kevin <feisty0516@vip.qq.com>

--- model/misc/vibrator/driver/src/vibrator_driver.c
+++ model/misc/vibrator/driver/src/vibrator_driver.c
@@ -44,6 +44,7 @@ void VibratorTimerEntry(uintptr_t para)
 {
     struct VibratorDriverData *drvData = (struct VibratorDriverData *)para;
 
+    (void)OsalMutexLock(&drvData->mutex);
     drvData->state = VIBRATOR_STATE_STOP;
     (void)OsalMutexUnlock(&drvData->mutex);
 
