10439baaf8684174f7f3e1a34c814aa0abda1f18
zfsamzfsamn <dulanqiongs359@163.com>
2022-04-21 14:57:39 +0800

    fix the compatibility problem of 64-bit kernel and 32-bit user mode
    Signed-off-by: yuanbo <yuanbo@huawei.com>

--- core/shared/include/hdf_io_service.h
+++ core/shared/include/hdf_io_service.h
@@ -20,8 +20,8 @@ extern "C" {
 #define DEV_MGR_NODE "dev_mgr"
 #define MAX_MODE_SIZE 0777
 #define DEV_NODE_PATH_MODE 0755
-#define HDF_WRITE_READ _IOWR('b', 1, struct HdfSBuf*)
-#define HDF_READ_DEV_EVENT _IOR('b', 2, struct HdfSBuf*)
+#define HDF_WRITE_READ _IO('b', 1)
+#define HDF_READ_DEV_EVENT _IO('b', 2)
 #define HDF_LISTEN_EVENT_START _IO('b', 3)
 #define HDF_LISTEN_EVENT_STOP _IO('b', 4)
 #define HDF_LISTEN_EVENT_WAKEUP _IO('b', 5)
@@ -34,13 +34,13 @@ typedef enum {
 } DevMgrCmd;
 
 struct HdfWriteReadBuf {
-    int cmdCode;
-    size_t writeSize;     // bytes to write
-    size_t writeConsumed; // bytes consumed by driver (for ERESTARTSYS)
-    uintptr_t writeBuffer;
-    size_t readSize; // bytes to read
-    size_t readConsumed; // bytes consumed by driver (for ERESTARTSYS)
-    uintptr_t readBuffer;
+    uint32_t writeSize;     // bytes to write
+    uint32_t writeConsumed; // bytes consumed by driver (for ERESTARTSYS)
+    uint64_t writeBuffer;
+    uint32_t readSize; // bytes to read
+    uint32_t readConsumed; // bytes consumed by driver (for ERESTARTSYS)
+    uint64_t readBuffer;
+    int32_t cmdCode;
 };
 
 struct HdfIoService *HdfIoServicePublish(const char *serviceName, uint32_t mode);
