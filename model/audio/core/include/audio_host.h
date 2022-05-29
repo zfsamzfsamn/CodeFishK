0a137686b83e3da98d8deaa08d948e2fd987908f
zfsamzfsamn <dulanqiongs359@163.com>
2022-05-30 02:55:49 +0800

    modify date
    Signed-off-by: vb6174 <zhangyunhu@huawei.com>

--- model/audio/core/include/audio_host.h
+++ model/audio/core/include/audio_host.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2021 Huawei Device Co., Ltd.
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
  *
  * HDF is dual licensed: you can use it either under the terms of
  * the GPL, or the BSD license, at your option.
