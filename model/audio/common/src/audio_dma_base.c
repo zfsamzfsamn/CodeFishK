d3be03cd556ab5485769e3cbfab9ee4e2d569e1a
zfsamzfsamn <dulanqiongs359@163.com>
2024-11-10 09:40:17 +0800

    code check
    Signed-off-by: vb6174 <zhangyunhu@huawei.com>

--- model/audio/common/src/audio_dma_base.c
+++ model/audio/common/src/audio_dma_base.c
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ *
+ * HDF is dual licensed: you can use it either under the terms of
+ * the GPL, or the BSD license, at your option.
+ * See the LICENSE file in the root of this repository for complete details.
+ */
 #include "audio_platform_if.h"
 
 int32_t AudioDmaBufAlloc(struct PlatformData *data, enum AudioStreamType streamType)
