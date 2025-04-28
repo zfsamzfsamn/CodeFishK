3c244170aa228cf0b51918c61aececaba8da22cb
zfsamzfsamn <dulanqiongs359@163.com>
2025-04-28 17:07:28 +0800

    Description: p2p模块拆分 Feature or Bugfix:Feature Binary Source: No
    Signed-off-by: YOUR_NAME <fangdong7@huawei.com>

--- model/network/wifi/core/components/p2p/p2p.h
+++ model/network/wifi/core/components/p2p/p2p.h
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2020-2021 Huawei Device Co., Ltd.
+ *
+ * HDF is dual licensed: you can use it either under the terms of
+ * the GPL, or the BSD license, at your option.
+ * See the LICENSE file in the root of this repository for complete details.
+ */
+
 #ifndef HDFLITE_P2P_H
 #define HDFLITE_P2P_H
 
