a54be131e7025d21ca638fd1d8181269d6121903
zfsamzfsamn <dulanqiongs359@163.com>
2026-09-06 13:29:53 +0800

    support rk3568 power-key
    Signed-off-by: liu_wei01 <liu_wei01@hoperun.com>

--- model/input/driver/hdf_input_device_manager.c
+++ model/input/driver/hdf_input_device_manager.c
@@ -18,7 +18,7 @@
 #define SERVICE_NAME_LEN     24
 #define INPUT_DEV_EXIST      1
 #define INPUT_DEV_NOT_EXIST  0
-#define INPUTDEV_FIRST_ID    1
+#define INPUTDEV_FIRST_ID    2
 #define FILLER_FLAG          1
 #define PLACEHOLDER_LENGTH   2
 #define PLACEHOLDER_LIMIT    10
@@ -35,6 +35,11 @@ static bool IsHidDevice(uint32_t devType)
     if ((devType > INDEV_TYPE_HID_BEGIN_POS) && (devType < INDEV_TYPE_UNKNOWN)) {
         return true;
     }
+#ifdef CONFIG_ARCH_ROCKCHIP
+    else if (devType == INDEV_TYPE_KEY) {
+        return true;
+    }
+#endif
     return false;
 }
 
